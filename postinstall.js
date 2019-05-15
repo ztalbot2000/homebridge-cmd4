38237e1516e5fa3f1161d6f82507a51c436b07c9
John Talbot <ztalbot2000@gmail.com>
2019-05-15 11:27:18 -0400

    update post install notes for version 2.1.2

--- postinstall.js
+++ postinstall.js
@@ -31,8 +31,8 @@ if (package.version.includes('1.2.3') ||
 if (package.version.includes('1.3.0') ||
     package.version.includes('1.3.1'))
 {
-  console.log("\n%sHomeBridge-Cmd4%s Version 1.3.0 Notes", KUND, KEND);
-  console.log("%sWARNING%s: The included config.json file has been changed to fix an issue with spacing in the name 'My v1 Fan' to 'My_v1Fan'.", KYEL, KEND);
+  console.log("\n%sHomeBridge-Cmd4 Version 1.3.0 Notes:%s", KUND, KEND);
+  console.log("   %sWARNING%s: The included config.json file has been changed to fix an issue with spacing in the name 'My v1 Fan' to 'My_v1Fan'.", KYEL, KEND);
   console.log("Spaces in the accessory name alters the number of shell command line arguments and should be used with caution; please copy it to:");
   console.log("%s  $HOME/.homebridge/%s", KGRN, KEND);
   console.log("or take the appropriate actions.");
@@ -40,7 +40,7 @@ if (package.version.includes('1.3.0') ||
   console.log("This version adds in functionality of fakegato-history for the Eve app.\n");
   console.log("Fakegato-history is quite new.  Please inform me if there is problems as fakegato-history evolves.\n");
   console.log("\n");
-  console.log("\n%sHomeBridge-Cmd4%s Version 1.3.1 Notes", KUND, KEND);
+  console.log("\n%sHomeBridge-Cmd4%s Version 1.3.1 Notes:", KUND, KEND);
   console.log("This version makes constants out of various types which fixes a few internal issues.\n");
   console.log("If upgrading from versions less than 1.3.0, See notes above.\n");
   console.log("\n");
@@ -48,18 +48,33 @@ if (package.version.includes('1.3.0') ||
   console.log("   John Talbot");
   console.log("\nP.S. If you really enjoy homebridge-cmd4, please star it on GitHub\n");
 }
+
 if (package.version.includes('2.0.0') ||
-    package.version.includes('2.1.0'))
+    package.version.includes('2.1.0') ||
+    package.version.includes('2.1.2'))
 {
-  console.log("\n%sHomeBridge-Cmd4%s Version 2.0 Notes", KUND, KEND);
-  console.log("This version changes the philosophy of homebridge-Cmd4 from Accessories that have known characteristics as per the HAP Spec to assigning any characteristic to any Accessory.\n");
-  console.log("By default only the known characteristics are assigned to each accessory, achieving the prevous results.");
+  console.log("\n%sHomeBridge-Cmd4 Version 2.0 Notes:%s", KUND, KEND);
+  console.log("   This version changes the philosophy of homebridge-Cmd4 from Accessories that have known characteristics as per the HAP Spec to assigning any characteristic to any Accessory.\n");
+  console.log("By default only the required characteristics are assigned to each accessory, achieving the prevous results.");
   console.log("For the more adventurous though, you may be able to configure accessories with new capabilities !!!");
-  console.log("\nThe format of the config.json file is the same, except for the recently added fakegato services. See the README.md file for the new rules. They are more aligned with fakegato and also more configurable.");
-  console.log("\nYou will find that Cmd4 2.0 is much more configurable than the past with 160 characteristics possible, compared to the previous 100 and the ability to poll for anything at any interval to be much more convenient.");
+  console.log("\nThe format of the config.json file is the same, except for the recently added fakegato services. See the README.md file for the new rules. They are more aligned with fakegato.");
+  console.log("\nYou will find that version 2.0 is much more configurable than the past with 60 new characteristics , totalling 160 possibilities and the ability to poll for anything at any interval to be much more convenient.");
   console.log("\nFor those with their own scripts, nothing changes as the format sent and received is exactly the same.");
-  console.log("\nThis new version also supports, TV, TV Speaker, Input Source, Irrigation, Faucet and many others.  You will need to copy the included config.json file and State.js file to their appropriate locations.");
-  console.log("\nEnjoy");
+  console.log("\nThis new version also supports, TV, TV Speaker, Input Source, Irrigation, Faucet and many others.  You will need to copy the included config.json file and State.js file to their appropriate locations to see these new devices.");
+
+  console.log("\n\n%sHomeBridge-Cmd4 Version 2.1.0 Notes:%s", KUND, KEND);
+  console.log("   This minor version removes 5000 plus lines of code for a more reliable lookup table of characteristics and services. No new functionality was added.");
+
+  console.log("\n\n%sHomeBridge-Cmd4 Version 2.1.2 Notes:%s", KUND, KEND);
+  console.log("   This minor version adds in 2796 passing unit tests and completes the lookup table, reducing yet more code and further increasing reliability. See the README for more information on unit testing.  The unit tests caught some difficiencies that require you to copy tHe State.js and config.json files from the distribution to their respective locations, if you use them..\n");
+  console.log("   %sWARNING%s: The included config.json and State.js file has been changed to fix issues found through unit testing in this version.", KYEL, KEND);
+
+  console.log("\nVersion 1.3.1 - 5194 lines of code");
+  console.log("Version 2.0   - 8576 lines of code");
+  console.log("Version 2.1   - 2825 lines of code");
+  console.log("Version 2.1.2 - 2635 lines of code");
+
+  console.log("\n\nEnjoy");
   console.log("   John Talbot");
   console.log("\nP.S. If you really enjoy homebridge-cmd4, please star it on GitHub\n");
 }
