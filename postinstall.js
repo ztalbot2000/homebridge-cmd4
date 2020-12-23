ca0c781df5e13c3889c4d76e5c486babaa10d9aa
John Talbot <ztalbot2000@gmail.com>
2020-12-23 01:19:00 -0500

    Tell people of changes

--- postinstall.js
+++ postinstall.js
@@ -15,13 +15,12 @@ KEND="\033[0m";
 
 const package = require('./package.json')
 
-if (package.version.includes('2.4.0') ||
-    package.version.includes('2.4.0'))
+if (package.version.includes('3.0.0') ||
+    package.version.includes('3.0.0'))
 {
-   console.log("\n%sHomeBridge-Cmd4 Version 2.3.0 Notes:%s\n", KUND, KEND);
+   console.log("\n%sHomeBridge-Cmd4 Version 3.0.0 Notes:%s\n", KUND, KEND);
    console.log("%s * %s For new users, you will need to follow the README to continue the configuration of HomeBridge-CMD4.\n", KGRN, KEND);
-   console.log("%s * %s For creating your own scripts, Please look at some of the examples in the 'Examples' sub-directory of this installed plugin or the extensively commented State.js script.\n", KGRN, KEND);
-   console.log("%s * %s If you are updating to this newer version of CMD4, The change logs are now all included in a separate ChangeLog file found with this plugin or on GitHub.\n", KGRN, KEND);
+   console.log("%s * %s With this version, it is *%sVery%s* important that you read the Changelogs.\n", KGRN, KEND, KMAG, KEND);
    console.log("\n\n   As always, if you like this plugin, don't forget to star it on GitHub.\n");
    console.log("\n   Enjoy");
    console.log("      John Talbot\n\n");
