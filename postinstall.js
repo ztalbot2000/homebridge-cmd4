59f34c1bb5e35f61abad5a2ae9da482eb5614466
John Talbot <ztalbot2000@gmail.com>
2021-01-11 19:44:45 -0500

    postinstall not getting executed

--- postinstall.js
+++ postinstall.js
@@ -7,9 +7,11 @@ const chalk = require( "chalk" );
 const package = require( "./package.json")
 
 if (package.version.includes( "3.0.0" ) ||
-    package.version.includes( "3.0.0" ))
+    package.version.includes( "3.0.2" ) ||
+    package.version.includes( "3.0.3" )
+   )
 {
-   console.log( chalk.underline( `HomeBridge-Cmd4 Version 3.0.0 Notes:\n` ) );
+   console.log( chalk.underline( `HomeBridge-Cmd4 Version 3.0.0+ Notes:\n` ) );
    console.log( chalk.green( `* ` ) + `For new users, you will need to follow the README to continue the configuration of HomeBridge-CMD4.\n` );
    console.log( chalk.green( `* ` ) + `With this version, it is ` + chalk.red( `*Very* ` ) + `important that you read the Changelogs as there are some new options avaliable..\n` );
    console.log(`\n   As always, if you like this plugin, don't forget to star it on GitHub.\n`);
