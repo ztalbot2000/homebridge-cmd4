4aa6ba87451738ac2847979a5ea48b2ffa911957
John Talbot <ztalbot2000@gmail.com>
2021-01-19 17:08:52 -0500

    Auto Generated doc tool now has perms

--- test/CMD4_ACC_TYPE_ENUM.js
+++ test/CMD4_ACC_TYPE_ENUM.js
@@ -44,7 +44,42 @@ describe( "Testing INITIALIZED CMD4_ACC_TYPE_ENUM", ( ) =>
    // Init the library for all to use
    let CMD4_ACC_TYPE_ENUM = ACC_DATA.init( _api.hap.Characteristic );
 
-   // *** TEST CMD4_ACC_TYPE_ENUM.properties[].service *******
+
+   describe("Testing Initialized CMD4_ACC_TYPE_ENUM.properties[]", ( ) =>
+   {
+      it('CMD4_ACC_TYPE_ENUM.properties should be an object', ( ) =>
+      {
+         assert.isObject(CMD4_ACC_TYPE_ENUM.properties, "CMD4_DEVICE_TYPE_ENUM.properties is not an object" );
+      });
+
+      it("Testing CMD4_ACC_TYPE_ENUM.properties[]", ( ) =>
+      {
+         for (let index=0; index < CMD4_ACC_TYPE_ENUM.EOL; index ++ )
+         {
+            let result = CMD4_ACC_TYPE_ENUM.properties[ index ];
+
+            // Make sure our properties are defined
+            assert.isNotNull( result, "CMD4_ACC_TYPE_ENUM.properties[" + index + "] is null. result: " + result );
+         }
+      });
+
+   });
+
+   describe("Testing Initialized CMD4_ACC_TYPE_ENUM.properties[].type", ( ) =>
+   {
+      for (let index=0; index < CMD4_ACC_TYPE_ENUM.EOL; index ++ )
+      {
+         let result = CMD4_ACC_TYPE_ENUM.properties[ index ].type;
+
+         it("CMD4_ACC_TYPE_ENUM.properties[" + index + "].type should not be a string", ( ) =>
+         {
+            assert.isString(result, "type is not a string at index" + index + " result: " + typeof result );
+         });
+      }
+   });
+
+
+   // *** TEST CMD4_ACC_TYPE_ENUM.properties[].characteristic *******
    describe('Testing Initialized CMD4_ACC_TYPE_ENUM.properties[].characteristic', ( ) =>
    {
       it('Testing CMD4_ACC_TYPE_ENUM.properties[].characteristic ', ( ) =>
@@ -87,6 +122,186 @@ describe( "Testing INITIALIZED CMD4_ACC_TYPE_ENUM", ( ) =>
          }
       });
    });
-});
 
+   // TJOS STIFF ADDED
+
+   // ** TEST ACC_TYPE_ENUM.properties[].props  **
+   describe('Testing CMD4_ACC_TYPE_ENUM.properties[].props', ( ) =>
+   {
+      for (let accTypeEnumIndex=0; accTypeEnumIndex < CMD4_ACC_TYPE_ENUM.EOL; accTypeEnumIndex ++ )
+      {
+          if (accTypeEnumIndex == CMD4_ACC_TYPE_ENUM.Name )
+             continue;
+
+          // Get the properties for this characteristic type
+          let accProperties = CMD4_ACC_TYPE_ENUM.properties[accTypeEnumIndex];
+          // Characteristics dont seem to get removed and homebridge put a limit
+          // of 100 Characteristics per service, so just create a new service
+          // per characteristic.  This is unit testing anyway, so not an issue.
+          let serviceName = 'Service' + accTypeEnumIndex;
+          let service = new _api.hap.Service(serviceName, _api.hap.uuid.generate(serviceName ), serviceName );
+
+          it('Creating service to test Characteristic', ( ) =>
+          {
+             assert.isNotNull(service, 'Service is null at accTypeEnumIndex: ' + accTypeEnumIndex );
+          });
+
+          describe('Testing CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + ']:' + accProperties.type, ( ) =>
+          {
+             var characteristic = CMD4_ACC_TYPE_ENUM.properties[accTypeEnumIndex].characteristic;
+             var verifyCharacteristic = CMD4_ACC_TYPE_ENUM.properties[accTypeEnumIndex].verifyCharacteristic;
+
+             it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].verifyCharacteristic', ( ) =>
+             {
+                assert.isDefined(verifyCharacteristic, 'verifyCharacteristic is not defined' );
+             });
+
+             service.addCharacteristic(characteristic );
+
+             let hapProps = service.getCharacteristic(characteristic ).props;
+             service.removeCharacteristic(characteristic );
+
+             it('props for HomeBridge Characteristic:' + accProperties.type,
+             ( ) =>
+             {
+                assert.isNotNull(hapProps, 'perms for Characteristic type:' + accProperties.type + ' is null' );
+             });
+
+             let hapFormat = hapProps.format;
+
+             // Test both Formats
+             it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.format should be the same', ( ) =>
+             {
+                assert.equal(hapFormat, accProperties.props.format, 'format:' + accProperties.props.format + ' not equal to expected ' + hapFormat );
+             });
+
+             // Test units
+             let hapUnits = hapProps.units;
+
+             it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.units should be the same', ( ) =>
+             {
+                assert.equal(hapUnits, accProperties.props.units, 'units:' + accProperties.props.units + ' not equal to expected ' + hapUnits );
+             });
+
+             // test minValue
+             let hapMinValue = hapProps.minValue;
+
+             it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.minValue should be the same', ( ) =>
+             {
+                 assert.equal(hapMinValue, accProperties.props.minValue, 'minValue:' + accProperties.props.minValue + ' not equal to expected ' + hapMinValue );
+             });
+
+             // test maxValue
+             let hapMaxValue = hapProps.maxValue;
+
+             if ( accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.CurrentHeatingCoolingState )
+             {
+
+                it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.maxValue should be the same', ( ) =>
+                {
+                   assert.equal(hapMaxValue, accProperties.props.maxValue, 'maxValue:' + accProperties.props.maxValue + ' not equal to expected ' + hapMaxValue );
+                });
+             } else {
+                //console.log("Homebridge is wrong, skipping" );
+             }
+
+             // test minStep
+             let hapMinStep = hapProps.minStep;
+
+             it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.minStep should be the same', ( ) =>
+             {
+                assert.equal(hapMinStep, accProperties.props.minStep, 'minStep:' + accProperties.props.minStep + ' not equal to expected ' + hapMinStep );
+             });
+
+             // test all perms (Except Version)
+             let hapPerms = hapProps.perms;
+             if ( accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.Version )
+             {
+                // Test if same number of perms
+                it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.perms.length should be the same', ( ) =>
+                {
+                   assert.equal(hapPerms.length, accProperties.props.perms.length, 'props.perms.length:' + accProperties.props.perms.length + ' not equal to expected ' + hapPerms.length );
+                });
+
+                for ( let permIndex = 0; permIndex < hapPerms.length; permIndex++ )
+                {
+                   let hapPerm = hapPerms[permIndex];
+
+                   it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].props.perms[' + permIndex + '] should be the same', ( ) =>
+                   {
+                      assert.equal(hapPerm, accProperties.props.perms[permIndex], 'props.perms[' + permIndex + ']:' + accProperties.props.perms[permIndex] + ' not equal to expected ' + hapPerm );
+                   });
+                }
+             } else {
+                // HAP has varing perms.  Bug report issued
+             }
+
+             // Our validValues should not be null
+             it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].validValues should not be null', ( ) =>
+             {
+                assert.isNotNull(accProperties.validValues, 'validValues is Null' );
+             });
+
+             // test all validValues
+             let hapValidValues = hapProps.validValues;
+
+             if ( ! hapValidValues )
+             {
+                // We defined what valid values are for BOOL
+                // so TRUE/FALSE can be a constant.
+                if (hapFormat != _api.hap.Characteristic.Formats.BOOL )
+                {
+                   // Hap has values defined, but not in valid values. We added them in ours
+                   if ( accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.CameraOperatingModeIndicator &&
+                        accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.ThirdPartyCameraActive &&
+                        accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.RecordingAudioActive &&
+                        accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.PeriodicSnapshotsActive &&
+                        accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.HomeKitCameraActive &&
+                        accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.EventSnapshotsActive )
+                   {
+                      // Test if our validValues is empty
+                      it('Our CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].validValues.length should be 0', ( ) =>
+                      {
+                         assert.equal(Object.keys(accProperties.validValues ).length, 0, 'validValuesh for: ' + accTypeEnumIndex + ' is not empty' );
+                      });
+                   } else {
+                      // console.log("Homebridge is wrong, skipping" );
+                   }
+                }
+             } else
+             {
+                // For InputDeviceType, we define the UNKNOWN_6 they do not. its okay
+                // For ManagedNetworkEnable, we define the UNKNOWN they do not. its okay
+                if ( accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.CurrentHeatingCoolingState &&
+                     accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.InputDeviceType &&
+                     accTypeEnumIndex != CMD4_ACC_TYPE_ENUM.ManagedNetworkEnable )
+                {
+                   // Test if same number of validValues
+                   it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].perms.length should be the same', ( ) =>
+                   {
+                      assert.equal(hapValidValues.length, Object.keys(accProperties.validValues ).length,   'validValues.length(' + Object.keys(accProperties.validValues ).length +' ) for: ' + accTypeEnumIndex + ' is not:' + hapValidValues.length );
+                   });
+
+                   // test all validValues against hapValidValues
+                   for ( let valuesIndex = 0; valuesIndex < hapValidValues.length; valuesIndex++ )
+                   {
+                       let hapValue = hapValidValues[valuesIndex];
+
+                       it('CMD4_ACC_TYPE_ENUM.properties[' + accTypeEnumIndex + '].validValues[' + valuesIndex + '] should be the same', ( ) =>
+                       {
+                             let cmd4Key = Object.keys(accProperties.validValues )[valuesIndex];
+                             let cmd4Value = accProperties.validValues[cmd4Key];
+                             assert.equal(hapValue, cmd4Value, 'validValues[' + valuesIndex + ']:' +   cmd4Value + ' not equal to expected ' + hapValue );
+                       });
+                   }
+                } else {
+                   // console.log("Homebridge is wrong, skipping" );
+                }
+             }
+             service.removeCharacteristic(characteristic );
+
+          });
+       }
+   });
+});
 
