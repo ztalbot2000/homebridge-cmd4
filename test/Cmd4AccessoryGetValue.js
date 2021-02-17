442e6beb463f56f11107518a779bf48e83c8bc64
John Talbot <ztalbot2000@gmail.com>
2021-02-17 12:38:26 -0500

    test: Check for stderr messages being logged

--- test/Cmd4AccessoryGetValue.js
+++ test/Cmd4AccessoryGetValue.js
@@ -10,10 +10,10 @@ var HomebridgeAPI = require( "../node_modules/homebridge/lib/api" ).HomebridgeAP
 var _api = new HomebridgeAPI(); // object we feed to Plugins
 
 
-// Need homebridge logging for Cmd4Accessory
+let hook = captureConsole();
 var logger_1 = require("../node_modules/homebridge/lib/logger");
+const log = logger_1.Logger.internal;
 Object.defineProperty(exports, "LogLevel", { enumerable: true, get: function () { return logger_1.LogLevel; } });
-
 
 
 // Init the library for all to use
@@ -72,38 +72,49 @@ describe( "Testing Cmd4Accessory", function( )
    it( "Test init Cmd4Accessory", function( )
    {
       // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
 
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       expect( cmd4Accessory ).to.be.a.instanceOf( Cmd4Accessory, "Cmd4Accessory is not an instance of Cmd4Accessory" );
+
+      // Clear the hook buffer for next time.
+      hook.reset();
    });
 
    it( "Test Cmd4Accessory.getValue", function( )
    {
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
       TVConfig.state_cmd = "./echoScripts/echo_ACTIVE";
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
+      hook.stop( );
 
       assert.isFunction( cmd4Accessory.getValue, "Cmd4Accessory.getValue is not a function" );
+
+      // Clear the hook buffer for next time.
+      hook.reset();
    });
 
    it( "Test Cmd4Accessory.getValue", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_ACTIVE";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       var clock = sinon.useFakeTimers();
       var callback = sinon.fake();
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, callback );
+      hook.stop( );
 
       clock.tick(99);
 
       assert(callback.notCalled, " getValue callback should only be updated once. Expected: 1 to equal: " + callback.callCount);
+
+      // Clear the hook buffer for next time.
+      hook.reset();
 
       done();
    });
@@ -111,17 +122,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Active should inject 1 to Hombridge for ACTIVE response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_ACTIVE";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
-      let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
+      hook.start();
+      let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 1;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -130,17 +144,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Active should inject 0 to Hombridge for INACTIVE response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_INACTIVE";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
-      let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
+      hook.start();
+      let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 0;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -149,16 +166,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Active should inject 0 to Hombridge for 0 response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_0";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 0;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -167,16 +188,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Active should inject 1 to Hombridge for 1 response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_1";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 1;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -185,16 +210,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Active should inject 0 to Hombridge for quoted0 response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_quoted0";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 0;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -203,16 +232,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Active should inject 1 to Hombridge for quoted1 response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_quoted1";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Active, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 1;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -221,16 +254,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Mute should inject false to Hombridge for false response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_false";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = false;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -239,16 +276,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Mute should inject true to Hombridge for true response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_true";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = true;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -257,16 +298,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Mute should inject false to Hombridge for 0 response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_0";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = false;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -275,16 +320,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Mute should inject true to Hombridge for 1 response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_1";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = true;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -293,16 +342,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Mute should inject false to Hombridge for quotedFalse response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_quotedFALSE";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = false;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -311,15 +364,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue Mute should inject true to Hombridge for quotedTrue response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_quotedTRUE";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = true;
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -328,16 +386,19 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue ClosedCaptions should inject 0 to Hombridge for DISABLED response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_DISABLED";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.ClosedCaptions, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 0;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -346,16 +407,20 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue ClosedCaptions should inject 1 to Hombridge for Enabled response", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_ENABLED";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.ClosedCaptions, function( rc, result )
       {
+         hook.stop();
+
          let expectedResult = 1;
-         //console.log( "rc=%s result=%s", rc, result);
 
          assert.equal(result, expectedResult, " getValue expected" + expectedResult + " received: " + result );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
@@ -363,74 +428,146 @@ describe( "Testing Cmd4Accessory", function( )
    it( "getValue of empty response should fail correctly", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_nothing";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
+         let errMsg= hook.capturedErr();
+         let expectedResult = `getValue: Mute function for: My_Television returned an empty string ""`;
 
          assert.notEqual(rc, 0, " getValue expected: not zero received: " + rc );
+
+         assert.match(errMsg, RegExp( expectedResult ), " getValue expected" + expectedResult + " received: " + errMsg );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
    });
+
    it( "getValue of null response should fail correctly", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_null";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
+         let errMsg= hook.capturedErr();
+         let expectedResult = `getValue: "null" returned from stdout for Mute My_Television`;
 
          assert.notEqual(rc, 0, " getValue expected: not zero received: " + rc );
+
+         assert.match(errMsg, RegExp( expectedResult ), " getValue expected" + expectedResult + " received: " + errMsg );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
    });
+
    it( "getValue of rc=1 response should fail correctly", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_true_withRcOf1";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
+         let errMsg= hook.capturedErr();
+         let expectedResult = `getValue Mute function failed for My_Television cmd: ./test/echoScripts/echo_true_withRcOf1 Get 'My_Television' 'Mute' Failed.  Error: Command failed: ./test/echoScripts/echo_true_withRcOf1 Get 'My_Television' 'Mute'`;
 
          assert.notEqual(rc, 0, " getValue expected: not zero received: " + rc );
+
+         assert.match(errMsg, RegExp( expectedResult ), " getValue expected" + expectedResult + " received: " + errMsg );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
    });
+
    it( "getValue of quoted Null should fail correctly", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_quotedNULL";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
+         let errMsg= hook.capturedErr();
+         let expectedResult = `getValue: Mute function for My_Television returned the string ""NULL""`;
 
          assert.notEqual(rc, 0, " getValue expected: not zero received: " + rc );
+
+         assert.match(errMsg, RegExp( expectedResult ), " getValue expected" + expectedResult + " received: " + errMsg );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
    });
+
    it( "getValue of quoted Nothing should fail correctly", function ( done )
    {
       TVConfig.state_cmd = "./test/echoScripts/echo_quotedNothing";
-      // logger_1.setDebugEnabled();
-      const log = logger_1.Logger.internal;
+
+      hook.start();
       let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
 
       cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
       {
+         hook.stop();
+
+         let errMsg= hook.capturedErr();
+         let expectedResult = `getValue: Mute function for: My_Television returned an empty string "" ""`;
 
          assert.notEqual(rc, 0, " getValue expected: not zero received: " + rc );
+
+         assert.match(errMsg, RegExp( expectedResult ), " getValue expected" + expectedResult + " received: " + errMsg );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
+
+         done();
+      });
+   });
+
+   it( "getValue of Nothing to stdout and something to stderr should show error message", function ( done )
+   {
+      TVConfig.state_cmd = "./test/echoScripts/echo_errorToStderr";
+
+      hook.start();
+      let cmd4Accessory = new Cmd4Accessory( log, TVConfig, _api, null );
+
+      cmd4Accessory.getValue( CMD4_ACC_TYPE_ENUM.Mute, function( rc, result )
+      {
+         hook.stop();
+
+         let errMsg= hook.capturedErr();
+         let expectedResult = "This message goes to stderr";
+
+         assert.match(errMsg, RegExp( expectedResult ), " getValue expected" + expectedResult + " received: " + errMsg );
+
+         // Clear the hook buffer for next time.
+         hook.reset();
 
          done();
       });
