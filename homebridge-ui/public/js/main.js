d1b94fbf78a4e0b6714ee5ec1504620ce1a56671
John Talbot <ztalbot2000@gmail.com>
2021-10-02 09:41:35 -0400

    feat: ui

---
+++ homebridge-ui/public/js/main.js
@@ -0,0 +1,583 @@
+/*global $, window, location, homebridge, schema*/
+
+
+
+const GLOBAL =
+{
+   pluginConfig: false,
+   customSchema: false,
+   accessoryOptions: false
+};
+
+
+async function createCustomSchema( accessory )
+{
+   console.log( "In CreateCustomSchema for:%s %s", accessory.name, accessory.displayName );
+   console.log( "In CreateCustomSchema accessory:%s", accessory );
+   //let settings = await homebridge.request( "/cmd4StaticVariable", "settings" );
+
+   GLOBAL.accessorySchema =
+   {
+      name: accessory.name,
+      displayName: accessory.displayName,
+      accessoryCharacteristics: accessory.characteristics,
+      polling: accessory.polling,
+      timeout: accessory.timeout,
+      queue: accessory.queue,
+      stateChangeResponseTime: accessory.stateChangeResponseTime,
+      stateCmdPrefix: accessory.stateCmdPrefix,
+      stateCmd: accessory.stateCmd,
+      stateCmdSuffix: accessory.stateCmdSuffix
+   };
+
+   GLOBAL.customSchema = homebridge.createForm( schema,
+   {
+      name: GLOBAL.pluginConfig[0].name,
+      debug: GLOBAL.pluginConfig[0].debug,
+      outputConstants: GLOBAL.pluginConfig[0].outputConstants,
+      statusMsg: GLOBAL.pluginConfig[0].statusMsg,
+      timeout: GLOBAL.pluginConfig[0].timeout,
+      stateChangeResponseTime: GLOBAL.pluginConfig[0].stateChangeResponseTime,
+      accessories: [],
+      accessories: accessory
+   } );
+
+   GLOBAL.customSchema.onChange( async config =>
+   {
+      console.log( "In customSchema.onChange config:%s %s", config );
+      //let settings = await homebridge.request( "/cmd4StaticVariable", "settings" );
+
+      GLOBAL.pluginConfig[0].name = config.name;
+      GLOBAL.pluginConfig[0].debug = config.debug;
+      GLOBAL.pluginConfig[0].outputConstants = config.outputConstants;
+      GLOBAL.pluginConfig[0].statusMsg = config.statusMsg;
+      GLOBAL.pluginConfig[0].timeout = config.timeout;
+      GLOBAL.pluginConfig[0].stateChangeResponseTime = config.stateChangeResponseTime;
+      GLOBAL.pluginConfig[0].accessories = GLOBAL.pluginConfig[0].accessories.map( accessory =>
+      {
+         if ( accessory.name === config.accessories.name )
+         {
+            accessory = config.accessories;
+         }
+         return accessory;
+      } );
+
+      try
+      {
+
+         await homebridge.updatePluginConfig( GLOBAL.pluginConfig );
+
+      }
+      catch( err )
+      {
+
+         homebridge.toast.error( err.message, 'Error' );
+
+      }
+
+   } );
+
+   return;
+
+}
+
+// This shows below the edit accessory. Not what I want
+async function createCustomSchema( accessory )
+{
+
+   GLOBAL.accessoryOptions = {
+      name:  accessory.name,
+      displayName:  accessory.displayName,
+      polling:  accessory.polling,
+   };
+
+   GLOBAL.customSchema = homebridge.createForm(schema, {
+      name: GLOBAL.pluginConfig[0].name,
+      debug: GLOBAL.pluginConfig[0].debug,
+      accessories: accessory
+   });
+
+   GLOBAL.customSchema.onChange(async config =>
+   {
+
+      GLOBAL.pluginConfig[0].name = config.name;
+      GLOBAL.pluginConfig[0].debug = config.debug;
+      GLOBAL.pluginConfig[0].accessories = GLOBAL.pluginConfig[0].accessories.map(accessory =>
+      {
+         if ( accessory.displayName === config.accessory.displayName &&
+              accessory.name === config.accessory.name )
+         {
+            accessory = config.accessory;
+         }
+         return accessory;
+      });
+
+      try {
+
+         await homebridge.updatePluginConfig(GLOBAL.pluginConfig);
+
+      } catch(err) {
+
+         homebridge.toast.error(err.message, 'Error');
+
+      }
+  });
+
+  return;
+
+}
+
+
+function resetUI( )
+{
+   console.log( "In resetUI" );
+
+   resetForm( );
+   resetSchema( );
+
+   return;
+
+}
+
+function resetForm( )
+{
+   console.log( "In resetForm" );
+
+   $( '#accessoryName' ).val( '' );
+   $( '#accessoryCharacteristics' ).val( '' );
+   $( '#accessoryPolling' ).val( '' );
+   $( '#authCode' ).val( '' );
+   $( '#authToken' ).val( '' );
+   $( '#authRefreshToken' ).val( '' );
+   $( '#authTokenType' ).val( '' );
+   $( '#authExpiresIn' ).val( '' );
+   $( '#authExpiresAt' ).val( '' );
+
+   $( '#codeInput' ).hide( );
+   $( '#tokenInput' ).hide( );
+
+   GLOBAL.accessorySchema = false;
+
+   return;
+
+}
+
+function resetSchema( )
+{
+   console.log( "In resetSchema" );
+
+   if ( GLOBAL.customSchema )
+   {
+      GLOBAL.customSchema.end( );
+      GLOBAL.customSchema = false;
+   }
+
+   return;
+
+}
+
+function addAccessoryToList( accessory )
+{
+   console.log( "In addAccessoryToList accessory.displayName:%s", accessory.displayName );
+
+   let name = typeof accessory === 'string' ? accessory : accessory.name;
+   $( '#accessorySelect' ).append( '<option value="' + name + '">'+ name + '</option>' );
+
+   return;
+
+}
+
+function removeAccessoryFromList( accessory )
+{
+   console.log( "In removeAccessoryToList accessory.displayName:%s", accessory.displayName );
+
+   let name = typeof accessory === 'string' ? accessory : accessory.name;
+   $( '#accessorySelect option[value=\'' + name + '\']' ).remove( );
+
+   return;
+
+}
+
+async function addNewDeviceToConfig( accessory )
+{
+   console.log( "In addNewDeviceToConfig for:%s %s", accessory.name, accessory.displayName );
+
+   let found = false;
+
+   try
+   {
+      const config =
+      {
+         name: accessory.name,
+         accessoryCharacteristics: accessory.characteristics,
+         polling: accessory.polling,
+         electricVehicle: false,
+         tankBatteryType: 'HUMIDITY',
+         token:
+         {
+            access_token: accessory.token.access_token,
+               refresh_token: accessory.token.refresh_token,
+               token_type: accessory.token.token_type,
+               expires_in: accessory.token.expires_in,
+               expires_at: accessory.token.expires_at
+         }
+      };
+
+      for( const acc in GLOBAL.pluginConfig[0].accessories )
+      {
+         if ( GLOBAL.pluginConfig[ 0 ].accessories[ acc ].name === accessory.name )
+         {
+            found = true;
+            GLOBAL.pluginConfig[ 0 ].accessories[ acc ].token =
+            {
+               access_token: accessory.token.access_token,
+               refresh_token: accessory.token.refresh_token,
+               token_type: accessory.token.token_type,
+               expires_in: accessory.token.expires_in,
+               expires_at: accessory.token.expires_at
+            };
+            homebridge.toast.success( accessory.name + ' refreshed!', 'Success' );
+         }
+      }
+
+      if ( !found )
+      {
+
+         GLOBAL.pluginConfig[0].accessories.push( config );
+         addAccessoryToList( config );
+
+         homebridge.toast.success( config.name + ' added to config!', 'Success' );
+
+      }
+
+      await homebridge.updatePluginConfig( GLOBAL.pluginConfig );
+      await homebridge.savePluginConfig( );
+
+   }
+   catch( err )
+   {
+
+      homebridge.toast.error( err.message, 'Error' );
+
+   }
+
+   return;
+
+}
+
+async function removeDeviceFromConfig( )
+{
+
+   let foundIndex;
+   let pluginConfigBkp = GLOBAL.pluginConfig;
+   let selectedAccessory = $( '#accessorySelect option:selected' ).text( );
+
+   GLOBAL.pluginConfig[0].accessories.forEach( ( accessory, index ) =>
+   {
+      if ( accessory.name === selectedAccessory )
+      {
+         foundIndex = index;
+      }
+   } );
+
+   if ( foundIndex !== undefined )
+   {
+
+      try
+      {
+
+         GLOBAL.pluginConfig[0].accessories.splice( foundIndex, 1 );
+
+         await homebridge.updatePluginConfig( GLOBAL.pluginConfig );
+         await homebridge.savePluginConfig( );
+
+         removeAccessoryFromList( selectedAccessory );
+
+         homebridge.toast.success( selectedAccessory + ' removed from config!', 'Success' );
+
+      }
+      catch( err )
+      {
+
+         GLOBAL.pluginConfig = pluginConfigBkp;
+
+         throw err;
+
+      }
+
+   }
+   else
+   {
+
+      throw new Error( 'No accessory found in config to remove!' );
+
+   }
+
+
+
+   return;
+
+}
+async function startButtonPressed( )
+{
+   let pluginConfig = await homebridge.getPluginConfig( );
+   GLOBAL.pluginConfig = pluginConfig;
+   console.log("got pluginConfig start sending start");
+
+   homebridge.request( "/startButtonPressed", pluginConfig );
+}
+async function backButtonPressed( )
+{
+   console.log("main.js async function back sending back");
+   homebridge.request( "/backButtonPressed" );
+}
+async function showConfigureGlobalsPageButtonPressed( )
+{
+   console.log("main.js async function globals sending globals");
+   homebridge.request( "/showConfigureGlobalsPage" );
+}
+async function showQueueGlobalsPageButtonPressed( )
+{
+   console.log("main.js async function globals sending globals");
+   homebridge.request( "/showQueueGlobalsPage" );
+}
+async function deleteQueueButtonPressed( value  )
+{
+   console.log("main.js async function deleteQueueButtonPressed :%s", value);
+}
+
+// STARTUP CODE
+( async ( ) =>
+{
+
+   try
+   {
+      // PAGE SWITCH EVENT
+      homebridge.addEventListener('my-pageEvent', (event) =>
+      {
+         console.log("In Main.js server.js told me from:%s to:%s", event.data.from, event.data.to);
+
+          $( event.data.from ).hide( );
+          $( event.data.to ).show( );
+      });
+
+      // UPDATE accessoryTypeSelect
+      let select = document.getElementById( "accessoryTypeSelect" );
+      console.log("main.js select=%s", select);
+
+      console.log( "In Cmd4.js select" );
+      let CMD4_DEVICE_TYPE_ENUM = await homebridge.request( "/cmd4StaticVariable", "CMD4_DEVICE_TYPE_ENUM" );
+      Object.keys( CMD4_DEVICE_TYPE_ENUM.properties ).forEach( key => {
+         let defaultSelection = false;
+         if ( key == CMD4_DEVICE_TYPE_ENUM.Switch )
+         {
+            console.log( " Setting default selection to true at: %s", key );
+            defaultSelection = true;
+         }
+         //console.log( "In Cmd4.js select appending %s", CMD4_DEVICE_TYPE_ENUM.properties[key].deviceName );
+         select.appendChild( new Option( CMD4_DEVICE_TYPE_ENUM.properties[key].deviceName, CMD4_DEVICE_TYPE_ENUM.properties[key].deviceName, defaultSelection, defaultSelection ) );
+      } );
+
+
+      GLOBAL.pluginConfig = await homebridge.getPluginConfig( );
+      //GLOBAL.pluginConfig.forEach( ( elem, index ) =>
+      //{
+         //console.log( "GLOBAL.pluginConfig[ %s ] returned by homebridge: %s", index, elem );
+      //} );
+
+      if ( !GLOBAL.pluginConfig.length )
+      {
+         let settings = await homebridge.request( '/cmd4StaticVariable', "settings" );
+
+         GLOBAL.pluginConfig = [
+         {
+            platform: settings.PLATFORM_NAME,
+            name: settings.PLATFORM_NAME,
+            debug: false,
+            outputConstants: settings.DEFAULT_OUTPUTCONSTANTS,
+            statusMsg: settings.DEFAULT_STATUSMSG,
+            timeout: settings.DEFAULT_TIMEOUT,
+            stateChangeResponseTime: settings.DEFAULT_STATE_CHANGE_RESPONSE_TIME,
+            accessories: []
+         }];
+
+      }
+      else
+      {
+
+         if ( !GLOBAL.pluginConfig[0].accessories || ( GLOBAL.pluginConfig[0].accessories && !GLOBAL.pluginConfig[0].accessories.length ) )
+         {
+            GLOBAL.pluginConfig[0].accessories = [];
+            // return transPage( false, $( '#notConfigured' ) );
+         }
+
+         GLOBAL.pluginConfig[0].accessories.forEach( accessory =>
+         {
+            $( '#accessorySelect' ).append( '<option value="' + accessory.name + '">'+ accessory.name + '</option>' );
+         } );
+         console.log("GLOBAL.pluginConfig[0]=%s", GLOBAL.pluginConfig[0] );
+         console.log("GLOBAL.pluginConfig[0].queueTypes=%s", GLOBAL.pluginConfig[0].queueTypes );
+
+         if ( GLOBAL.pluginConfig[0].queueTypes )
+         {
+            $( "#globalQueueMessage" ).hide( );
+            $( "#globalQueueContainer" ).show( );
+            //document.getElementById("globalQueueMessage").style.visibility = "hidden";
+
+            GLOBAL.pluginConfig[0].queueTypes.forEach( entry =>
+            {
+               console.log("Adding queue: %s", entry.queue );
+              $( '#globalQueuesForm' ).append(
+                   '<div class="row no-gutters">' +
+                     '<div class="col">' +
+                        '<div class="card card-body">' +
+                           '<button type="submit" class="btn btn-primary p-0 mt-0 mb-0 deleteQueueButton" value="' + entry.queue +'">' +
+                              '<i class="fa fa-trash" aria-hidden="true"></i>' +
+                           '</button>' +
+                        '</div>' +
+                     '</div>' +
+                     '<div class="col">' +
+                        '<div class="card card-body">' + entry.queue + '</div>' +
+                     '</div>' +
+                     '<div class="col">' +
+                        '<div class="card card-body">' + entry.queueType + '</div>' +
+                     '</div>' +
+                  '</div>'
+              );
+            });
+         } else {
+            $( "#globalQueueMessage" ).show( );
+            $( "#globalQueueContainer" ).hide( );
+         }
+      }
+   }
+   catch( err )
+   {
+
+      homebridge.toast.error( err.message, 'Error' );
+
+   }
+
+} )( );
+
+//jquery listener
+
+$( '.backButton' ).on( 'click', ( ) =>
+{
+   console.log(" main.js click .back");
+   backButtonPressed();
+} );
+
+$( '#startButton' ).on( 'click', ( ) =>
+{
+   console.log("Click startButton");
+   startButtonPressed();
+});
+
+$( '.showConfigureGlobalsPageButton' ).on( 'click', ( ) =>
+{
+   console.log("Click showConfigureGlobalsPageButton");
+   showConfigureGlobalsPageButtonPressed();
+} );
+
+$( '.showQueueGlobalsPageButton' ).on( 'click', ( ) =>
+{
+   console.log("Click showQueueGlobalsPageButton");
+   showQueueGlobalsPageButtonPressed();
+} );
+
+$( '.configureNewQueuePageButton' ).on( 'click', ( ) =>
+{
+   console.log("Click configureNewQueuePageButton");
+   configureNewQueuePageButtonPressed();
+} );
+
+$( '.deleteQueueButton' ).on( 'click', ( value ) =>
+{
+   console.log("Click configureNewQueuePageButton value:%s", value );
+   deleteQueueButtonPressed( value);
+} );
+
+async function populateSelect( )
+{
+   let select = document.getElementById( "accessoryType" );
+
+   console.log( "In Cmd4.js select" );
+   let CMD4_DEVICE_TYPE_ENUM = await homebridge.request( "/cmd4StaticVariable", "CMD4_DEVICE_TYPE_ENUM" );
+   Object.keys( CMD4_DEVICE_TYPE_ENUM.properties ).forEach( key => {
+      let defaultSelection = false;
+      if ( key == CMD4_DEVICE_TYPE_ENUM.Switch )
+      {
+         console.log( " Setting default selection to true at: %s", key );
+         defaultSelection = true;
+      }
+      select.appendChild( new Option( CMD4_DEVICE_TYPE_ENUM.properties[key].deviceName, CMD4_DEVICE_TYPE_ENUM.properties[key].deviceName, defaultSelection, defaultSelection ) );
+   } );
+}
+
+$( '#addAccessory, #Xstart' ).on( 'click', ( ) =>
+{
+
+   resetUI( );
+
+   populateSelect( );
+
+
+   //let activeContent = $( '#notConfigured' ).css( 'display' ) !== 'none' ? $( '#notConfigured' ) : $( '#isConfigured' );
+
+} );
+
+$( '#editAccessory' ).on( 'click', ( ) =>
+{
+
+   resetUI( );
+
+   let selectedAccessory = $( '#accessorySelect option:selected' ).text( );
+   let accessory = GLOBAL.pluginConfig[0].accessories.find( accessory => accessory.name === selectedAccessory );
+
+   if ( !accessory )
+      return homebridge.toast.error( 'Can not find the accessory!', 'Error' );
+
+   console.log("EDIT ACCESSORY INCOMPLETE");
+   createCustomSchema( accessory );
+
+
+} );
+
+$( '#refreshAccessory' ).on( 'click', async ( ) =>
+{
+
+   if ( GLOBAL.customSchema && GLOBAL.accessorySchema )
+   {
+
+      resetSchema( );
+
+      let accessory = GLOBAL.pluginConfig[0].accessories.find( accessory => accessory.name === GLOBAL.accessorySchema.name );
+
+      if ( !accessory )
+         return homebridge.toast.error( 'Can not find accessory in config!', 'Error' );
+
+   }
+
+} );
+
+$( '#removeAccessory' ).on( 'click', async ( ) =>
+{
+
+   try
+   {
+
+      await removeDeviceFromConfig( );
+
+      resetUI( );
+
+   }
+   catch ( err )
+   {
+
+      homebridge.toast.error( err.message, 'Error' );
+
+   }
+
+} );
+
---
+++ homebridge-ui/server.js
@@ -0,0 +1,281 @@
+/* jshint node: true,esversion: 9, -W014, -W033 */
+/* eslint-disable new-cap */
+'use strict'
+
+const { HomebridgePluginUiServer } = require('@homebridge/plugin-ui-utils');
+const { RequestError } = require('@homebridge/plugin-ui-utils');
+const fs = require('fs')
+
+// Settings, Globals and Constants
+let settings = require( "../cmd4Settings" );
+const constants = require( "../cmd4Constants" );
+
+
+// These would already be initialized by index.js
+let CMD4_ACC_TYPE_ENUM = require( "../lib/CMD4_ACC_TYPE_ENUM" );
+let CMD4_DEVICE_TYPE_ENUM = require( "../lib/CMD4_DEVICE_TYPE_ENUM" );
+
+
+//orig const { AuthorizationCode } = require('simple-oauth2');
+
+class UiServer extends HomebridgePluginUiServer
+{
+   constructor ()
+   {
+
+      super();
+      console.log("settings=%s", settings.PLUGIN_NAME);
+
+
+      // Allow main.js to access Cmd4 Static variables as html files cannot
+      // require Cmd4 javascript files
+      this.onRequest('/startButtonPressed', this.startButtonPressed.bind(this));
+      this.onRequest('/backButtonPressed', this.backButtonPressed.bind(this));
+      this.onRequest('/showConfigureGlobalsPage', this.showConfigureGlobalsPage.bind(this));
+      this.onRequest('/showQueueGlobalsPage', this.showQueueGlobalsPage.bind(this));
+
+
+
+      this.onRequest('/getCachedAccessories', this.getCachedAccessories.bind(this));
+
+      this.onRequest('/cmd4StaticVariable', this.cmd4StaticVariable.bind(this));
+      this.onRequest('/ACC_Info', this.cmd4AccInfo.bind(this));
+      this.onRequest('/DEVICE_Info', this.cmd4DeviceInfo.bind(this));
+
+      this.fromPage = "#start";
+      this.toPage = null;
+      this.pages = [ ];
+
+      this.pluginConfig = null;
+
+      this.cachedAccessories = this.getCachedAccessories;
+
+      this.ready();
+   }
+
+   async createSchemaForExistingAccessory( accessory )
+   {
+      let schema=
+      {
+          name:
+          {
+             title: "Name",
+             description: "Name of accessory",
+             type: "string",
+             default: accessory.name,
+             required: true
+          },
+          displayName:
+          {
+             title: "DisplayNamw",
+             description: "Name of accessory",
+             type: "string",
+             default: accessory.displayNameame,
+             required: true
+          },
+          manufacturer:
+          {
+             name: "Manufacturer",
+             type: "string",
+             placeholder: accessory.manufacturer || "",
+             description: "Set the manufacturer name for display in the Home app.",
+             required: false
+          },
+          model:
+          {
+             name: "Model",
+             type: "string",
+             placeholder: accessory.model || "",
+             description: "Set the modal name for display in the Home app.",
+             required: false
+          },
+          serialNumber:
+          {
+             name: "SerialNumber",
+             type: "string",
+             placeholder: accessory.serialNumber || "",
+             description: "Set the serial number for display in the Home app.",
+             required: false
+          },
+          stateChangeResponseTime:
+          {
+             name: "StateChangeResponseTime",
+             type: "nummber",
+             min: 0,
+             max: 300,
+             placeholder: accessory.stateChangeResponseTime,
+             description: "Set the accessory's state chane response time (sec).",
+             required: false
+          },
+          timeout:
+          {
+             name: "Timeout",
+             type: "nummber",
+             min: 0,
+             max: 300,
+             placeholder: accessory.timeout,
+             description: "Set the accessory's timeout (msec).",
+             required: true
+          }
+      };
+      if ( typeof accessory.polling == "boolean" )
+      {
+          schema.polling =
+          {
+             title: "Polling",
+             type: "boolean",
+             default: accessory.polling,
+             description: "Polling interval in seconds.",
+             required: false
+          };
+      } else {
+          schema.polling =
+          {
+             title: "Polling",
+             type: "array",
+             items: {
+             }
+          }
+          accessory.polling.forEach( ( entry, index ) =>
+          {
+             schema.polling.items[ index ] =
+             {
+                title: "Characteristic",
+                type: "string",
+                placeholder: entry.characteristic,
+                required: true
+             },
+             {
+                title: "Timeout",
+                type: "number",
+                min: 0,
+                max: 300,
+                placeholder: entry.timeout,
+                description: "Set the accessory's timeout (msec).",
+                required: false
+             },
+             {
+                title: "StateChangeResponseTime",
+                type: "number",
+                min: 0,
+                max: 300,
+                placeholder: entry.stateChangeResponseTime,
+                description: "Set the accessory's state chane response time (sec).",
+                required: false
+             };
+          });
+      }
+   }
+
+     // A native method getCachedAccessories() was introduced in config-ui-x v4.37.0
+   // The following is for users who have a lower version of config-ui-x
+   async getCachedAccessories( )
+   {
+      try {
+         // Define the plugin and create the array to return
+         const plugin = settings.PLUGIN_NAME;
+         const devicesToReturn = []
+
+         // The path and file of the cached accessories
+         const accFile = this.homebridgeStoragePath + '/accessories/cachedAccessories'
+
+         // Check the file exists
+         if (fs.existsSync(accFile)) {
+           // Read the cached accessories file
+           let cachedAccessories = await fs.promises.readFile(accFile)
+
+           // Parse the JSON
+           cachedAccessories = JSON.parse(cachedAccessories)
+
+           // We only want the accessories for this plugin
+           cachedAccessories
+             .filter(accessory => accessory.plugin === plugin)
+             .forEach(accessory => devicesToReturn.push(accessory))
+         }
+
+         // Return the array
+         return devicesToReturn
+       } catch (err) {
+         // Just return an empty accessory list in case of any errors
+         return []
+       }
+   }
+
+
+   // A method for main.js to access Static Cmd4 variables
+   async cmd4StaticVariable( variableString )
+   {
+      //console.log("server.js for %s returning: %s", variableString, eval(variableString));
+      console.log("server.js for %s", variableString );
+      return eval( variableString );
+   }
+   async cmd4AccInfo( accTypeEnumIndex )
+   {
+      return CMD4_ACC_TYPE_ENUM.properties[ accTypeEnumIndex ];
+   }
+   async cmd4DeviceInfo( deviceTypeEnumIndex )
+   {
+      return CMD4_DEVICE_TYPE_ENUM.properties[ deviceTypeEnumIndex ];
+   }
+
+   async startButtonPressed( pluginConfig )
+   {
+      console.log("Server.js in start() pluginConfig: %s", pluginConfig );
+      console.log("Server.js in start() pluginConfig.length: %s", pluginConfig.length );
+      this.pluginConfig = pluginConfig;
+      this.pluginConfig.forEach( ( entry, index ) =>
+      {
+          console.log("Server.js pluginConfig[ %s ]: %s", index, entry );
+      });
+
+      let fromPage = "#start";
+      let toPage = "";
+      if ( pluginConfig && pluginConfig.length >= 1 && pluginConfig[0].accessories.length >= 1 )
+         toPage = "#selectExistingAccessory";
+      else
+         toPage = "#newAccessory";
+
+      this.pages.push( fromPage );
+      this.pages.push( toPage );
+      this.pushEvent('my-pageEvent', { from: fromPage,   // from: is hide
+                                       to:   toPage }    // to: is show
+                    );
+
+   }
+   async backButtonPressed( )
+   {
+      let fromPage = this.pages.pop();
+      let toPage = this.pages[ this.pages.length -1];
+      console.log("Server.js in back() toPage: %s fromPage", toPage, fromPage );
+      this.pushEvent('my-pageEvent', { from: fromPage,       // from: is hide
+                                       to:   toPage }        // to:   is show
+                    );
+   }
+   async showConfigureGlobalsPage( )
+   {
+      let fromPage = this.pages[ this.pages.length -1];
+      let toPage = "#configureGlobals";
+      console.log("Server.js in globals() toPage: %s fromPage", toPage, fromPage );
+
+      this.pages.push( toPage );
+      this.pushEvent('my-pageEvent', { from: fromPage,       // from: is hide
+                                       to:   toPage }        // to:   is show
+                    );
+   }
+   async showQueueGlobalsPage( )
+   {
+      let fromPage = this.pages[ this.pages.length -1];
+      let toPage = "#queueGlobalsPage";
+      console.log("Server.js toPage: %s fromPage", toPage, fromPage );
+
+      this.pages.push( toPage );
+      this.pushEvent('my-pageEvent', { from: fromPage,       // from: is hide
+                                       to:   toPage }        // to:   is show
+                    );
+   }
+}
+
+(() =>
+{
+   return new UiServer;
+})();
